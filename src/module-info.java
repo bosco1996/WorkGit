8a3605e58f29129a77dff2be695f2d1dcfc8bb52
bosco1996 <claretbosco12345@gmail.com>
2023-02-02 07:31:11 +0530

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author clare
+ *
+ */
+module PracticeGit {
+}
---
+++ src/org/base/GitPractice.java
@@ -0,0 +1,10 @@
+package org.base;
+
+public class GitPractice {
+	public static void main(String[] args) {
+		System.out.println("Git practice \n");
+		String s = "sun shines";
+		System.out.println(s);
+	}
+
+}
